a77ecd238194f4e8cdb98ee22ae33ea62f85cecd
Sandhya <144054124+sandhya-1234@users.noreply.github.com>
2024-01-19 21:22:48 +0530

    Add files via upload

---
+++ Angularjs/ascript.js
@@ -0,0 +1,10 @@
+var app = angular.module('myApp', []);
+app.controller('myController', function ($scope) {
+ $scope.students = [
+ { name: 'Akshaya', cgpa: 8.8 },
+ { name: 'Krishna Patel', cgpa: 7.2 },
+ { name: 'Johnson', cgpa: 6.5 },
+ { name: 'Safwan', cgpa: 7.5},
+ { name: 'Zeenath', cgpa: 8.5}
+ ];
+});
