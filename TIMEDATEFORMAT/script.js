dd1d1f3ea69aa883e3a968c5bc62593f1bcccb85
Sandhya <144054124+Sandhya015@users.noreply.github.com>
2024-03-10 12:15:44 +0530

    Add files via upload

---
+++ TIMEDATEFORMAT/script.js
@@ -0,0 +1,10 @@
+
+var app = angular.module('myApp', []);
+app.controller('myController', function ($scope, $filter) {
+    $scope.selectedFormat = 'fullDate'; 
+    $scope.updateDate = function () {
+        var currentDate = new Date();
+        $scope.formattedDate = $filter('date')(currentDate, $scope.selectedFormat);
+    };
+    $scope.updateDate();
+});
